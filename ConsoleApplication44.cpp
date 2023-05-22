3b8bc4a289f9e9183e7e8160c83c4d871d9ad31c
cherif044 <124671952+cherif044@users.noreply.github.com>
2023-05-22 22:16:18 +0300

    Update ConsoleApplication44.cpp

--- ConsoleApplication44.cpp
+++ ConsoleApplication44.cpp
@@ -1,14 +1,74 @@
-// ConsoleApplication44.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 using namespace std;
+#include <algorithm>
+#include <list>
+
+
+void displayList(const std::list<int>& myList) {
+    for (const auto& element : myList) {
+        std::cout << element << " ";
+    }
+    std::cout << std::endl;
+}
+class graph
+{
+private:
+    int count;
+    pair<int, list<int>>* array;
+public:
+    graph(int temp[], int c)
+    {
+        count = c;
+        array = new pair<int, list<int>>[c];
+        for (int i = 0; i < c; i++)
+        {
+            array[i].first = temp[i];
+        }
+    }
+    void addedge(int x, int y)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            if (array[i].first == x)
+            {
+                array[i].second.push_back(y);
+            }
+            if (array[i].first == y)
+            {
+                array[i].second.push_back(x);
+            }
+        }
+    }
+
+    void print_adjecency_list()
+    {
+        for (int i = 0; i < count; i++)
+        {
+            cout << array[i].first << " : ";
+            displayList(array[i].second);
+        }
+    }
+};
+
+
+int main()
+{
+    int array[5] = { 1,2,3,4,5 };
+    graph x(array, 5);
+    x.addedge(1, 2);
+    x.addedge(1, 3);
+    x.addedge(2, 4);
+    x.addedge(2, 5);
+    x.print_adjecency_list();
+
+
+}
+
 struct node
 {
     node* left, * right;
     int data;
 };
-
 class BST
 {
 private:
@@ -22,7 +82,7 @@ public:
     {
         return roots;
     }
-    node* insert(node*root,int value)
+    node* insert(node* root, int value)
     {
         node* add = new node;
         add->data = value;
@@ -36,11 +96,11 @@ public:
         {
             if (value < root->data)
             {
-                root->left=insert(root->left, value);
+                root->left = insert(root->left, value);
             }
             else if (value > root->data)
             {
-                root->right=insert(root->right, value);
+                root->right = insert(root->right, value);
             }
         }
         return root;
@@ -50,15 +110,15 @@ public:
         roots = insert(roots, value);
     }
 
-    void display(node*r)
+    void display(node* r)
     {
         if (r == NULL)
             return;
         else
         {
-            cout << r->data<<"\t";
-            
-                display(r->left);
+            cout << r->data << "\t";
+
+            display(r->left);
             display(r->right);
         }
     }
@@ -78,18 +138,18 @@ public:
         }
         else if (value < r->data)
         {
-            return search(r->left,value);
+            return search(r->left, value);
         }
         else if (value > r->data)
         {
-            return search(r->right,value);
+            return search(r->right, value);
         }
     }
     bool search(int value)
     {
         return search(roots, value);
     }
-    node* min(node*r)
+    node* min(node* r)
     {
         if (r == NULL)
         {
@@ -102,7 +162,7 @@ public:
         else
         {
             return min(r->left);
-                
+
         }
     }
     node* max(node* r)
@@ -122,23 +182,421 @@ public:
         }
     }
 };
-int main()
+struct element
 {
-    BST x;
-    x.insert(5);
-    x.insert(7);
-    x.insert(4);
-    cout << x.min(x.getroot())->data;
+    string value;
+    int key;
+};
+class hashtable
+{
+private:
+    element* table;
+    int size;
+public:
+    hashtable(int siz)
+    {
+        size = siz;
+        table = new element[size];
+        for (int i = 0; i < size; i++)
+        {
+            table[i].value = "-1";
+            table[i].key = -1;
+        }
+    }
+    bool isfull()
+    {
+        for (int i = 0; i < size; i++)
+        {
+            if (table[i].value == "-1")
+                return false;
+        }
+        return true;
 
+    }
+
+    void insert(int k,string v)
+    {
+        element temp;
+        temp.value = v;
+        temp.key = k;
+        if (!isfull())
+        {
+            int index = k%size;
+            while (table[index].value != "-1")
+            {
+                index=(index+1) % size;
+            }
+            table[index] = temp;
+        }
+        else
+        {
+            cout << "table is full";
+        }
+    }
+    bool search(int k,string v)
+    {
+
+            int index = k % size;
+            while (index < size && index >= 0 && k % size!=index-1)
+            {
+                if (table[k%size].value == v)
+                    return true;
+                else if (table[k%size].value == "-1")
+                    return false;
+                else
+                    k++;
+            }
+
+    }
+    void display()
+    {
+        for (int i = 0; i < size; i++)
+        {
+            cout << i<<" :  key : " << table[i].key << " value : " << table[i].value << endl;
+        }
+
+    }
+};
+struct element
+{
+    element* next;
+    int value;
+};
+class linkedlist {
+private:
+    element* head, * prev, * cursor;
+public:
+    linkedlist()
+    {
+        head == NULL;
+        prev == NULL;
+        cursor = NULL;
+    }
+    bool isempty()
+    {
+        return (head == NULL);
+    }
+    bool iscursorempty()
+    {
+        return (cursor == NULL);
+    }
+    void tofirst()
+    {
+        cursor = head;
+        prev = NULL;
+    }
+    bool isatfirst()
+    {
+        return (cursor==head)
+    }
+    void advance()
+    {
+        prev = cursor;
+        cursor = cursor->next;
+    }
+    void toEnd()
+    {
+        if(!isempty())
+        while (cursor->next != NULL)
+        {
+            advance();
+        }
+    }
+    bool atEnd()
+    {
+        return cursor->next == NULL;
+    }
+    int getlistsize()
+    {
+        element* temp = head;
+        int count=0;
+        while (temp != NULL)
+        {
+            temp = temp->next;
+            count++;
+        }
+        return count;
+    }
+    void update_cursor(int v)
+    {
+        cursor->value = v;
+    }
+    void retrieve(int v)
+    {
+        v = cursor->value;
+    }
+    void insert_head(int v)
+    {
+        element* add;
+        add->value = v;
+        add->next = head;
+        head = add;
+        cursor = add;
+        prev = NULL;
+    }
+    void insert_after_cursor(int v)
+    {
+        element* add;
+        add->value = v;
+        if (atEnd())
+        {
+            cursor->next = add;
+            add->next = NULL;
+        }
+        else
+        {
+            add->next = cursor->next;
+            cursor->next = add;
+
+        }
+    }
+    void insert_before_cursor(int v)
+    {
+        element* add;
+        add->value = v;
+
+        if (isatfirst())
+        {
+            add->next = cursor;
+            head = add;
+            cursor = add;
+            prev = NULL;
+        }
+        else
+        {
+            add->next = cursor;
+            prev->next = add;
+            cursor = add;
+        }
+    }
+    void insert_end(int v)
+    {
+        toEnd();
+        element* add;
+        add->value = v;
+        cursor->next = add;
+        prev = cursor;
+        cursor = add;
+        add->next = NULL;
+    }
+    void delete_cursor()
+    {
+        element* q = cursor;
+        if (!iscursorempty())
+        {
+            if (isatfirst())
+            {
+
+                head = cursor->next;
+                cursor = head;
+                prev = NULL;
+                delete q;
+            }
+            else if(atEnd())
+            {
+                prev->next = NULL;
+                cursor = NULL;
+                delete q;
+            }
+            else
+            {
+                prev->next = cursor->next;
+                cursor = cursor->next;
+                delete q;
+            }
+        }
+
+    }
+    void deletehead()
+    {
+        if (!isempty())
+        {
+            tofirst();
+            delete_cursor();
+        }
+    }
+    void deleteEnd()
+    {
+        if (!isempty()) { toEnd(); delete_cursor(); }
+    }
+    void makelistempty()
+    {
+        tofirst();
+        while (!isempty())
+        {
+            delete_cursor();
+        }
+    }
+    bool search(int v)
+    {
+        element* temp = head;
+        bool found = false;
+        if (!isempty())
+        {
+            while (temp->next != NULL )
+            {
+                if (temp->value == v)
+                {
+                    found = true;
+                    cursor = temp;
+                }
+                temp = temp->next;
+
+            }
+        }
+        return found;
+    }
+    void (int v)
+    {
+        tofirst();
+        while ((cursor != NULL) && (v > cursor->value))
+        {
+            advance();
+        }
+        insert_before_cursor(v);
+
+    }
+    void display()
+    {
+        tofirst();
+        while (cursor != NULL)
+        {
+            cout << cursor->value << endl;
+            advance();
+        }
+    }
+    void rotate(int count)
+    {
+        element* end,*kth;
+        end = head;
+        kth = head;
+        int temp = 1;
+        while (end->next != NULL)
+        {
+            end = end->next;
+        }
+        while (temp < count && kth->next != NULL)
+        {
+            kth = kth->next;
+            temp++;
+        }
+        end->next = head;
+        head = kth->next;
+        kth->next = NULL;
+
+    }
+    void reverse()
+    {
+        element* current = head;
+        element* previous = NULL;
+        element* next = head->next;
+        element* temp;
+        while (current != NULL)
+        {
+            temp = current->next;
+            current->next = previous;
+            temp->next = current;
+        }
+
+    }
+
+};
+void selection_sort(int array[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = i + 1; j < size; j++)
+        {
+            if (array[i] > array[j])
+            {
+                swap(array[i], array[j]);
+            }
+        }
+    }
 }
+int partition(int array[], int start, int end)
+{
+    int pindex = start;
+    int pivot = array[end];
+    for (int i = start; i < end; i++)
+    {
+        if (array[i] < pivot)
+        {
+            swap(array[pindex], array[i]);
+            pindex++;
+        }
+    }
+    swap(array[pindex], array[end]);
+}
+quick_sort(int array[], int start, int end)
+{
+    if (start < end)
+    {
+        int pindex = partition(array, start, end);
+        quick_sort(array, start, pindex);
+        quick_sort(array, pindex + 1, end);
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    }
+}
+void merge_sort(int array[], int start,int end)
+{
+    if (start < end)
+    {
+        int middle = (start + end) / 2;
+        merge_sort(array, start, middle);
+        merge_sort(array, middle+1, end);
+        merge(array, start, middle, end);
+    }
+}
+void merge(int array, int start, int middle, int end)
+{
+    int i = start;
+    int j = middle + 1;
+    int k = start;
+    int* temp = new int[end-start];
+    while (i <= middle && j <= end)
+    {
+        if (array[i] < array[j])
+        {
+            temp[k] = array[i];
+            k++;
+            i++;
+        }
+        else
+        {
+            temp[k] = array[j];
+            k++;
+            j++;
+        }
+    }
+    while (i <= middle)
+    {
+        temp[k] = array[i];
+        k++;
+        i++;
+    }
+    while (j <= end)
+    {
+        temp[k] = array[j];
+        k++;
+        j++;
+    }
+    for (int j = start, j <= end; j++)
+    {
+        array[j] = temp[j];
+    }
+}
+void insertion_sort(int array[], int size)
+{
+    for (int i = 1; i < size; i++)
+    {
+        int temp = array[i];
+        int it = i - 1;
+        while (it >= 0 && temp < array[it])
+        {
+            array[it + 1] = array[it];
+            it--;
+        }
+        array[it + 1] = temp;
+    }
+}
